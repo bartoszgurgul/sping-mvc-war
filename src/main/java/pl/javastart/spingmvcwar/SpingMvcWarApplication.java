20ed277d190c6c23b5e2ecf3def55f260cb37acd
bartoszgurgul <bartoszgurgul@gmail.com>
2021-02-01 23:19:46 +0100

    Initial commit

---
+++ src/main/java/pl/javastart/spingmvcwar/SpingMvcWarApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.spingmvcwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingMvcWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingMvcWarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/spingmvcwar/SpingMvcWarApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.spingmvcwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingMvcWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
